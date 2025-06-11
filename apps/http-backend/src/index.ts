87fdd28ef44329201f4055eb21807a27db06331d
ASHIN T V <146656061+ashintv@users.noreply.github.com>
2025-06-11 21:13:08 +0530

    backend-protyped

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -108,4 +108,21 @@ app.post("/room", Auth, async (req, res) => {
         }
     }
 });
+
+app.get("/chat/:roomId" ,async (req,res)=>{
+    const roomId = Number(req.params.roomId)
+    const response = await prismaClient.chat.findMany({
+        where:{
+            roomId
+        },
+        orderBy:{
+            id:"desc"
+        },
+        take:50,
+
+    })
+
+    res.json({response})
+})
+
 app.listen(3001);
